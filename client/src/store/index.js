ccf9a64c17aa896a3d13363701e316ab75fee819
Or Assraf <orassraf@gmail.com>
2018-05-31 14:07:35 -0400

    npm i axios lodash vuetify vuex-persistedstate vuex-router-sync

---
+++ client/src/store/index.js
@@ -0,0 +1,21 @@
+import createPersistedStore from 'vuex-persistedstate';
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+    plugins: [
+        createPersistedStore(),
+    ],
+});
